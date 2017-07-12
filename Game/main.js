284ac7c1a33f3021b22bf2addcb954be6b827ec1
Dani <daniferrito2@hotmail.com>
2017-07-12 19:46:14 +0200

    Update main.js

--- Game/main.js
+++ Game/main.js
@@ -1,7 +1,36 @@
 var button = document.getElementById("boton");
 var text = document.getElementById("puntos");
 var puntos = 0;
+var cuadrado = document.getElementById("cuadrado");
+
+var posX = 50;
+var posY = 50;
 button.onclick = function(){
   puntos+=1;
   text.innerText = puntos;
   }
+  
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+
+    e = e || window.event;
+
+    if(e.key=="w"){
+		posY-=10;
+	}if(e.key=="a"){
+		posX-=10;
+	}if(e.key=="s"){
+		posY+=10;
+	}if(e.key=="d"){
+		posX+=10;
+	}
+	console.log(e)
+	
+	
+	cuadrado.style.left = posX+'px';
+	cuadrado.style.top  = posY+'px';
+}
+  
+
+
