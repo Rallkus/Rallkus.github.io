50ab77f8a49ed05c999caea35889ed46000ddf85
Segio <sergiohuertas1@hotmail.com>
2017-07-14 20:07:22 +0200

    El contador ahora cuenta los movimientos que haces y el botón teletransporta el cuadrado a un lugar random de la pantalla

--- Game/main.js
+++ Game/main.js
@@ -5,11 +5,17 @@ var cuadrado = document.getElementById("cuadrado");
 
 var posX = 50;
 var posY = 50;
+
 button.onclick = function(){
   puntos+=1;
   text.innerText = puntos;
+  posY=(Math.random()*1000%550)+50;
+  posX=(Math.random()*1000%1150)+50;
+
+  cuadrado.style.left = posX+'px';
+	cuadrado.style.top  = posY+'px';
   }
-  
+
 document.onkeydown = checkKey;
 
 function checkKey(e) {
@@ -18,19 +24,24 @@ function checkKey(e) {
 
     if(e.key=="w"){
 		posY-=10;
+    puntos+=1;
+    text.innerText = puntos;
 	}if(e.key=="a"){
 		posX-=10;
+    puntos+=1;
+    text.innerText = puntos;
 	}if(e.key=="s"){
 		posY+=10;
+    puntos+=1;
+    text.innerText = puntos;
 	}if(e.key=="d"){
 		posX+=10;
+    puntos+=1;
+    text.innerText = puntos;
 	}
 	console.log(e)
-	
-	
+
+
 	cuadrado.style.left = posX+'px';
 	cuadrado.style.top  = posY+'px';
 }
-  
-
-
